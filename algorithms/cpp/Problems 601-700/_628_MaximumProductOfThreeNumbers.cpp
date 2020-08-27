907455a0c76e691efa2be5db2942c3dc174bb8c7
shivamacs <arorashivam1212@gmail.com>
2020-08-27 23:38:32 +0530

    Update LeetCode 628

--- algorithms/cpp/Problems 601-700/_628_MaximumProductOfThreeNumbers.cpp
+++ algorithms/cpp/Problems 601-700/_628_MaximumProductOfThreeNumbers.cpp
@@ -5,14 +5,30 @@
 using namespace std;
 
 int maximumProduct(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
+    int n = nums.size(), min1, min2, max1, max2, max3;
+        
+    min1 = min2 = INT_MAX;
+    max1 = max2 = max3 = INT_MIN;
     
-    int n = nums.size(), pproduct = 1, nproduct = 0;
-                
-    pproduct = nums[n - 1] * nums[n - 2] * nums[n - 3];
-    nproduct = nums[0] * nums[1] * nums[n - 1];
+    for(int i = 0; i < n; i++) {
+        if(nums[i] < min1) {
+            min2 = min1;
+            min1 = nums[i];
+        } else if(nums[i] < min2)
+            min2 = nums[i];
+        
+        if(nums[i] > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = nums[i];
+        } else if(nums[i] > max2) {
+            max3 = max2;
+            max2 = nums[i];
+        } else if(nums[i] > max3)
+            max3 = nums[i];
+    }
     
-    return max(pproduct, nproduct);
+    return max(max1 * max2 * max3, min1 * min2 * max1);
 }
 
 int main() {
