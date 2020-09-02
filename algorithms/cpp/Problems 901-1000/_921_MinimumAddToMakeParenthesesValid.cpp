27df3e17780671ad655cb3b42450ca94bb170089
shivamacs <arorashivam1212@gmail.com>
2020-09-02 23:08:07 +0530

    Update LeetCode 921

--- algorithms/cpp/Problems 901-1000/_921_MinimumAddToMakeParenthesesValid.cpp
+++ algorithms/cpp/Problems 901-1000/_921_MinimumAddToMakeParenthesesValid.cpp
@@ -4,31 +4,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int minParenthesesAddition(string exp) {
-    int oc = 0, cc = 0;
-    stack<char> s;
+int minAddToMakeValid(string S) {
+    int n = S.length(), oc = 0, cc = 0;
+        
+    for(int i = 0; i < n; i++) {
+        if(S[i] == '(')
+            oc++;
+        else if(oc > 0)
+            oc--;
+        else
+            cc++;
+    } 
     
-    for (int i=0;i<exp.length();i++) {
-        if (exp[i] == ' ') continue;
-        if (s.empty() || exp[i] == '(') s.push(exp[i]);
-        else if (!s.empty() && (exp[i] == ')' && s.top() == '(')) s.pop();
-        else if (!s.empty() && (exp[i] == ')' && s.top() != '(' )) s.push(exp[i]);
-    }
-
-    while(!s.empty()) {
-        if (s.top() == '(') oc++;
-        else cc++;
-        s.pop();
-    }
-
     return oc + cc;
 }
 
 int main()
 {
-    string exp;
+    string S;
     cout<<"Enter parentheses expression: ";
-    getline(cin, exp);
-    int result = minParenthesesAddition(exp);
+    cin>>S;
+
+    int result = minAddToMakeValid(S);
     cout<<"Minimum number of parentheses to be added to make the expression valid: "<<result<<endl;
 }
