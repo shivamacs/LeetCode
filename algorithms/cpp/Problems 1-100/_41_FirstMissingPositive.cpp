4edc72e25cf539d6c74a4a24f555b4e8d20d148b
shivamacs <arorashivam1212@gmail.com>
2020-11-23 16:02:20 +0530

    Update Leetcode 41

--- algorithms/cpp/Problems 1-100/_41_FirstMissingPositive.cpp
+++ algorithms/cpp/Problems 1-100/_41_FirstMissingPositive.cpp
@@ -11,26 +11,17 @@ void swap(int &a, int &b) {
 }
 
 int firstMissingPositive(vector<int>& nums) {
-    if (nums.size() == 0)
-        return 1;
+    int n = nums.size();
+        
+    for(int i = 0; i < n; ++ i)
+        while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+            swap(nums[i], nums[nums[i] - 1]);
     
-    nums.push_back(-1);
+    for(int i = 0; i < n; ++ i)
+        if(nums[i] != i + 1)
+            return i + 1;
     
-    int n = nums.size(), i = 0;
-    
-    while(i < n) {
-        if (nums[i] != i && nums[i] > 0 && nums[i] < n && nums[i] != nums[nums[i]])
-            swap(nums[nums[i]], nums[i]);
-        else 
-            i++;
-    }
-            
-    for(int i = 1; i < n; i++) {
-        if (nums[i] != i)
-            return i;
-    }
-    
-    return n;
+    return n + 1;
 }
 
 int main()
